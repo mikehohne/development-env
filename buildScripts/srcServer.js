0c52f79fe6b28cf95576eef3a34e01e4238a96d2
Mike Hohne <mikehohne@gmail.com>
2017-10-22 18:47:12 -0600

    working on updating with vanilla js

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -15,14 +15,12 @@ app.use(require('webpack-dev-middleware')(compiler, {
 	publicPath: config.output.publicPath
 }));
 
-app.get('/', function(req,res){
+app.get('/', (req,res) => {
 	res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err){
-	if(err) {
+app.listen(port, (err) => {
+	if(err)
 		console.log(err)
-	} else {
-		open('http://localhost:' + port)
-	}
+	open('http://localhost:' + port)
 })
